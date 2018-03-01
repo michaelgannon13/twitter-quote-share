18a2ae2e6b0d22184ad687f88915f16cbaa44d3d
Michael gannon <michael.gannon13@gmail.com>
2018-03-01 19:54:49 +0000

    more

--- js/main.js
+++ js/main.js
@@ -1,17 +1,18 @@
 var currentCategory;
 currentCategory = conor;
 
-$(document).ready(function() {
-	$(document).keydown(function(e) {
-	  if (e.keyCode == '32') {
-		alert('space');
-	  }
-	});
-  });
-  
+
 function load() {
 	console.log("load called");
 	document.getElementById('quote').innerHTML = conor.quote;
 	document.getElementById('author').innerHTML = conor.author;
 	document.getElementById('image-behind').src = conor.authorImg;
+}
+
+function conorQuotes () {
+	console.log("conor clicked");
+}
+
+function changeDis() {
+	console.log("kanye clicked");
 }
