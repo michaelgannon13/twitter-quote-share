25fbc3143468922399ae5b3c6727ac86dab534ab
Michael gannon <michael.gannon13@gmail.com>
2018-03-01 19:44:49 +0000

    broken code out into files. changing data structure

---
+++ js/categories.js
@@ -0,0 +1,67 @@
+// var quotes = [
+// 	"'Doubt is removed by action'",
+// 	"'My Success isn’t the result of arrogance, it’s the result of belief'",
+// 	"'Talent does not exist, we are all equal as human beings'",
+// 	"'The more you seek the uncomfortable, the more you will become comfortable'",
+// 	"'What you think you become'"
+// ];
+
+// var authors = ["Conor McGregor", "Conor McGregor", "Conor McGregor", "Conor McGregor", "Buddha"];
+// var pics = ["conor.jpg", "conor2.jpeg", "conor3.jpg", "conor.jpg", "buddha.jpg"];
+
+// var mot = ["'There's only one thing that's impossible, and that's to beat a man who doesn't give up'", "'It's OK to be the greatest you'", "'The main thing people are controlled by is their perceptions of themselves'", "'Believe in your flyness, conquer your shyness'", "'Shoot for the stars, so if you fall you land on a cloud'"];
+// var motAuthors = ["Conor McGregor", "Kanye West", "Kanye West", "Kanye West", "Kanye West"];
+// var motPics = ["conor.jpg", "kanye1.png", "kanye2.png", "kanye3.jpg", "kanye4.jpg"];
+
+// var dis = ["'Discipline is the bridge between goals and accomplishment'", "'It's ok to be the greatest you'", "'The main thing people are controlled by is their perceptions of themselves'", "'Believe in your flyness, conquer your shyness'", "'Shoot for the stars, so if you fall you land on a cloud'"];
+// var disAuthors = ["Jim Rohn", "Kanye West", "Kanye West", "Kanye West", "Kanye West"];
+// var disPics = ["conor.jpg", "kanye1.png", "kanye2.png", "kanye3.jpg", "kanye4.jpg"];
+
+var conorMcGregor = "Conor McGregor", 
+    kanyeWest = "Kanye West";
+
+var conor = [
+    {
+      author: conorMcGregor,
+      quote: "'Doubt is removed by action'",
+      authorImg: "img/conor.jpg"
+    }, 
+    {
+      author: conorMcGregor,
+      quote: "'My Success isn’t the result of arrogance, it’s the result of belief'",
+      authorImg: "img/conor2.jpeg"
+    }, 
+    {
+      author: conorMcGregor,
+      quote: "'Talent does not exist, we are all equal as human beings'",
+      authorImg: "img/conor3.jpg"
+    }, 
+    {
+      author: conorMcGregor,
+      quote: "'The more you seek the uncomfortable, the more you will become comfortable'",
+      authorImg: "img/conor3.jpg"
+    }     
+  ]
+
+  var kanye = [
+    {
+      author: kanyeWest,
+      quote: "'It's OK to be the greatest you'",
+      authorImg: "img/kanye1.jpg"
+    }, 
+    {
+      author: kanyeWest,
+      quote: "'Shoot for the stars, so if you fall you land on a cloud'",
+      authorImg: "img/kanye2.jpeg"
+    }, 
+    {
+      author: kanyeWest,
+      quote: "'Believe in your flyness, conquer your shyness'",
+      authorImg: "img/kanye3.jpg"
+    }, 
+    {
+      author: kanyeWest,
+      quote: "'The main thing people are controlled by is their perceptions of themselves'",
+      authorImg: "img/kanye.jpg"
+    }     
+  ]
