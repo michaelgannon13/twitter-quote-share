ccdf03729ff515c110a76e81ab0f92acd908c87e
Michael gannon <michael.gannon13@gmail.com>
2018-03-10 17:58:40 +0000

    logs removed

--- js/main.js
+++ js/main.js
@@ -26,7 +26,6 @@ function loadKanye() {
 
 function nextQuote() {
 	if (counter !== currentPerson.length) {
-		console.log("counter is = " + counter + " currentPerson length is " + currentPerson.length);
 		document.getElementById('author').innerHTML = currentPerson[counter].author;
 		document.getElementById('quote').innerHTML = currentPerson[counter].quote;
 		document.getElementById('image-behind').src = currentPerson[counter].authorImg;
