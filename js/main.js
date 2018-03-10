d1754ae7a8d5cc7b724c5f418966dabcf9cf87dd
Michael gannon <michael.gannon13@gmail.com>
2018-03-10 16:49:45 +0000

    removed animate.css and fade function

--- js/main.js
+++ js/main.js
@@ -11,27 +11,17 @@ window.onkeydown = function (e) {
 };
 
 
-function loadConor() {	
+function loadConor() {
 	currentPerson = conor;
 }
 
-function loadKanye() {	
+function loadKanye() {
 	currentPerson = kanye;
 }
 
-function fade() {
-	$('#image-behind').css("opacity", "0");
-	$('#image-behind').css('opacity', .2).fadeIn("slow");
-}
-
 function nextQuote() {
-	if (counter !== currentPerson.length) {
-		fade();
-		counter++;
-		document.getElementById('author').innerHTML = currentPerson[counter].author;
-		document.getElementById('quote').innerHTML = currentPerson[counter].quote;
-		document.getElementById('image-behind').src = currentPerson[counter].authorImg;
-	} else {
-		counter = 0;
-	}
+	counter++;
+	document.getElementById('author').innerHTML = currentPerson[counter].author;
+	document.getElementById('quote').innerHTML = currentPerson[counter].quote;
+	document.getElementById('image-behind').src = currentPerson[counter].authorImg;
 }
