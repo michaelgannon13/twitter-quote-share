9ed47509b7b1f7c6661b4d2b030b74a0c2d2a289
Michael gannon <michael.gannon13@gmail.com>
2018-03-10 14:37:24 +0000

    adding fade function

--- js/main.js
+++ js/main.js
@@ -19,8 +19,14 @@ function loadKanye() {
 	currentPerson = kanye;
 }
 
+function fade() {
+	$('#image-behind').css("opacity", "0");
+	$('#image-behind').css('opacity', .2).fadeIn("slow");
+}
+
 function nextQuote() {
 	if (counter !== currentPerson.length) {
+		fade();
 		counter++;
 		document.getElementById('author').innerHTML = currentPerson[counter].author;
 		document.getElementById('quote').innerHTML = currentPerson[counter].quote;
